eba1a32738b210550c65a83e0ef3679793c7eeb2
SS2867 <songzl2003@gmail.com>
2024-02-06 23:44:26 +0800

    Demo1

--- Demo1_BasicRemoteControl_Car/main.c
+++ Demo1_BasicRemoteControl_Car/main.c
@@ -1,1 +1,69 @@
 #include "stm32f10x.h"                  // Device header
+#include "Motor.h"
+#include "utils.h"
+#include <string.h>
+#include <stdio.h>
+
+char rx_buffer[20] = "";
+char msg[20] = "";
+int joystick_x = 50;
+int joystick_y = 50;
+void USART2_IRQHandler() {
+	if(USART_GetITStatus(USART2, USART_IT_RXNE) != RESET) {
+		char ch = (unsigned char) USART_ReceiveData(USART2); 
+		for(int i=0; i<18; i++){rx_buffer[i] = rx_buffer[i+1]; }
+		rx_buffer[18] = ch;
+		msg[0] = ch;
+		
+		/*int opand = checkPositiveInteger(str_slice(rx_buffer, 18-2+1, 2));
+		if (opand!=-1){ 
+			if(str_is_equal(str_slice(rx_buffer, 18-4+1, 2), "/X")){
+				joystick_x = opand; Joystick_control(joystick_x, joystick_y);
+				sprintf(msg, "//Set joystick X %d//", opand);
+			}else if (str_is_equal(str_slice(rx_buffer, 18-4+1, 2), "/Y")){
+				joystick_y = opand; Joystick_control(joystick_x, joystick_y);
+				sprintf(msg, "//Set joystick Y %d//", opand);
+			}		
+		}*/
+		int opand = checkPositiveInteger_2char(rx_buffer[17], rx_buffer[18]);
+		if (opand!=-1 && rx_buffer[15]=='/'){ 
+			if(rx_buffer[16]=='X'){
+				joystick_x = opand; Joystick_control(joystick_x, joystick_y);
+				sprintf(msg, "//Set joystick X %d//", opand);
+			}else if (rx_buffer[16]=='Y'){
+				joystick_y = opand; Joystick_control(joystick_x, joystick_y);
+				sprintf(msg, "//Set joystick Y %d//", opand);
+			}		
+		}
+	}
+	if(USART_GetITStatus(USART2, USART_IT_TC) != RESET) {
+		if (msg[0] != 0){USART_SendData(USART2, msg[0]);}
+		for(int i=0; i<19; i++){msg[i] = msg[i+1];}
+		msg[19] = 0;
+	}
+}
+
+
+
+
+int sTicks = 0;
+void TIM2_IRQHandler(void){
+	if (TIM_GetITStatus(TIM2, TIM_IT_Update)!=RESET){
+		sTicks ++; if (sTicks>=1000){sprintf(msg, "D"); sTicks = 0;}
+		TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+		
+		
+	}
+}
+
+int main(){
+	motor_init();
+	USART2_Init();
+	strcpy(msg , "We are ready!");
+	while(1){
+		
+	}
+}
+
+
+
---
+++ Demo1_BasicRemoteControl_Car/utils.c
@@ -0,0 +1,100 @@
+#include "stm32f10x.h"                  // Device header
+#include <cstring>
+#include <cmath>
+#include <stdlib.h>
+
+int max(int a, int b){return (a>b)? a:b;}
+int min(int a, int b){return (a<b)? a:b;}
+
+void exhaustive_delay(int t) {           // delay by t ms
+    int i; int j;
+    for (i = 0; i < t; i++) {
+        for (j=0;j<2666 ;j++)
+        { GPIOA->BSRR ^= 0x00;} // do something to PA0 	 		
+    }
+}
+
+
+
+char* str_slice(char src[], int src_start, int length) {
+    char* sliced = (char*) malloc((length + 1) * sizeof(char));
+    memcpy(sliced, src + src_start, length);
+    sliced[length] = '\0';
+    return sliced;
+}
+
+int str_is_equal(char str1[], char str2[]){
+    return strcmp(str1, str2) == 0;
+}
+
+int checkPositiveInteger(char str[]) {
+	int num = 0;
+	int length = strlen(str);
+	if (length == 0){return -1;}
+	for (int i=0; i<length; i++){
+		if (str[length-i-1]<48 || str[length-i-1]>57) {return -1;}
+		num += pow(10, i) * (str[length-i-1]-48);
+	}
+	return num;
+}
+
+int checkPositiveInteger_2char(char ch1, char ch2) {
+	if (ch1<48 || ch1>57 || ch2<48 || ch2>57) {return -1;}
+	int num = 10*(ch1-48) + (ch2-48);
+	return num;
+}
+
+void USART2_Init(){
+	//USART2 TX RX
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_AFIO, ENABLE);
+	
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+	
+	// Tx pin
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+	// Rx pin
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_Init(GPIOA, &GPIO_InitStructure); 
+	
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2,ENABLE);
+	USART_InitTypeDef USART_InitStructure;
+	USART_InitStructure.USART_BaudRate = 115200;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+	USART_Init(USART2, &USART_InitStructure);
+	USART_Cmd(USART2, ENABLE);
+	
+	NVIC_InitTypeDef NVIC_InitStruct;
+	NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_InitStruct.NVIC_IRQChannel = EXTI15_10_IRQn;
+	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0x02;
+	NVIC_Init(&NVIC_InitStruct);
+	
+	NVIC_InitTypeDef NVIC_InitStructure;
+	// Enable the USART2 TX Interrupt 
+	USART_ITConfig(USART2, USART_IT_TC, ENABLE );
+	NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+
+	// Enable the USART2 RX Interrupt
+	USART_ITConfig(USART2, USART_IT_RXNE, ENABLE );
+	NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+}
